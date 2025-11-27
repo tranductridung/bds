565e1562e957eb23d03a92e516be471c599e0165
Tri Dung <tranductridung0103@gmail.com>
2025-11-27 22:55:52 +0700

    init project

---
+++ backend/src/main.ts
@@ -0,0 +1,44 @@
+import 'dotenv/config';
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: [
+      process.env.FRONTEND_URL || 'http://localhost:5173',
+      process.env.BACKEND_URL || 'http://localhost:3000',
+      /https:\/\/.*\.vercel\.app$/,
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API documentation for system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+}
+bootstrap();
